9fd502ddffb33ac9c2be68289a4b5563768eb580
leslievongphakdy <vongphakdyl@wit.edu>
2017-10-19 15:06:47 -0400

    Docs

--- Group 36 - Train Simulator/src/edu/wit/comp2000/group36/train/Passenger.java
+++ Group 36 - Train Simulator/src/edu/wit/comp2000/group36/train/Passenger.java
@@ -1,6 +1,15 @@
 package edu.wit.comp2000.group36.train;
 
+/**
+ * @author Leslie Vongphakdy
+ * COMP 2000 - 03
+ * Lab 3: Queues, Train Simulator
+ * Group 36
+ * Due: October 30, 2017
+ **/
+
 public class Passenger {
+	
 	private Station start;
 	private Station end;
 	private static int nextID = 1;
@@ -8,7 +17,15 @@ public class Passenger {
 	private boolean isInbound; //TODO: calculate isInbound
 	private boolean initialized = false;
 	
-	public Passenger(int ID, Station start, Station destination) {
+	/**
+	 * Gives every passenger a number id (aka their name).
+	 * Parameters start and destination set where the passenger 
+	 * is coming from and where they are going.
+	 * @param ID
+	 * @param start
+	 * @param end 
+	 **/
+	public Passenger(int ID, Station start, Station end) {
 		ID = nextID;
 		nextID++;
 		this.start = start;
@@ -16,6 +33,10 @@ public class Passenger {
 		initialized = true;
 	}
 	
+	/**
+	 * Checks the direction of the train.
+	 * @return if it's inbound, true. if it's outbound, false.
+	 **/
 	public boolean IsInbound() {
 		checkInitialization();
 		return isInbound;
@@ -28,20 +49,38 @@ public class Passenger {
 		} //end if
 	} // end checkInitialization
 	
+	/**
+	 * Calls and then returns id for passenger.
+	 * @return
+	 **/
 	public int getID() {
 		return ID;
 	}
 	
+	/**
+	 * Calls and returns Station start Location (id) 
+	 * from Station class.
+	 * @return
+	 **/
 	public Station getStart() {
 		return start;
 	}
 	
+	/**
+	 * Calls and returns Station destination Location (id)
+	 * from Station class.
+	 * @return
+	 **/
 	public Station getEnd() {
 		return end;
 	}
-	 
+	
+	/**
+	 * sets up output for Passenger's itinerary
+	 **/
 	public String toString() {
 		String result = " ";
+		result += "Ticket Itinerary:";
 		result += "Passenger " + ID + "\t";
 		result += "Station coming from is " + start.getLocation() + "\t";
 		result += "Station they're going to is " + end.getLocation() + "\t";
