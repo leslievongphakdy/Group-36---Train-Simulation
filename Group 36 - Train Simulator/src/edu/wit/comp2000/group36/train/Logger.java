5200b653e89246745f34e219806fdb689aa88401
leslievongphakdy <vongphakdyl@wit.edu>
2017-10-19 17:17:42 -0400

    Actual Logger

--- Group 36 - Train Simulator/src/edu/wit/comp2000/group36/train/Logger.java
+++ Group 36 - Train Simulator/src/edu/wit/comp2000/group36/train/Logger.java
@@ -1,41 +1,30 @@
 package edu.wit.comp2000.group36.train;
 
-import java.util.Random;
 import java.io.IOException;
-import java.util.logging.FileHandler;
-import java.util.logging.SimpleFormatter;
+import java.io.PrintWriter;
+
 
 public class Logger {
-	public static final Random RAND = new Random();
-
-	public static void main(String[] args) {
-
-		Logger logger = Logger.getLogger("Train Simulator Log");
-		FileHandler fh;
-
+	
+	private static PrintWriter pw;
+	
+	static {
+		
 		try {
-			fh = new FileHandler("C:/"); // insert file path
-			logger.addHandler(fh);
-			SimpleFormatter formatter = new SimpleFormatter();
-			fh.setFormatter(formatter);
-
-		} catch (SecurityException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
+			pw = new PrintWriter("TrainSimulator.log");
 		}
-
+		catch (IOException e){
+		}
+		
 	}
-
-	private static Logger getLogger(String string) {
-		// TODO Auto-generated method stub
-		return null;
+	
+	public static void logging(String s) {
+		pw.println(s);
+		pw.flush();
 	}
-
-	private void addHandler(FileHandler fh) {
-		// TODO Auto-generated method stub
-
+	
+	public static void main (String[]args) {
+		logging("my log");
 	}
-
 
 }
